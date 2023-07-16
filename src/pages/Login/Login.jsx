6729a39976f4a95d85a184d4f5a76c4b377156b6
jewelrana3 <juyelrana7752@gmail.com>
2023-07-16 15:32:24 +0600

    sign up and login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -40,76 +40,72 @@ const Login = () => {
       {/* <Helmet>
         <title>SpeckEasy | Login</title>
       </Helmet> */}
-      <div className="hero min-h-screen py-10">
-        <div className="hero-content flex-col w-full">
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold mb-5 text-orange-600">
-              Please Login!
-            </h1>
-          </div>
-          <div className="card flex-shrink-0 w-full max-w-md bg-slate-100">
-            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email*</span>
-                </label>
-                <input
-                  type="email"
-                  {...register("email", { required: true })}
-                  name="email"
-                  placeholder="email"
-                  className="input input-bordered"
-                />
-                {errors.email && <span className="text-red-600">required</span>}
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password*</span>
-                </label>
-                <div className="flex items-center justify-between">
-                  <input
-                    type={show ? "text" : "password"}
-                    {...register("password", {
-                      required: true,
-                    })}
-                    placeholder="password"
-                    className="input input-bordered relative w-full"
-                  />
-                  <span
-                    className="absolute right-10 p-2 cursor-pointer"
-                    onClick={() => setShow(!show)}
-                  >
-                    {show ? <FaEye /> : <FaEyeSlash />}
-                  </span>
-                </div>
-                <label className="label">
-                  <Link to='/forgetPass' className="label-text-alt link link-hover">
-                    Forgot password?
-                  </Link>
-                </label>
-              </div>
-              <div className="form-control mt-6">
-                <input
-                  className="btn text-white bg-orange-700 hover:bg-orange-800 border border-l-8 border-r-8 border-green-800 hover:border-orange-900"
-                  type="submit"
-                  value="Login"
-                />
-              </div>
-            </form>
-            <div className="text-center mb-10">
-              <p>
-                New to this site! Please{" "}
-                <Link to="/signup" className="hover:underline text-blue-500">
-                  Register
-                </Link>
-              </p>
-              <SocialLogin></SocialLogin>
-            </div>
-          </div>
+ <div className="hero py-10">
+  <div className="hero-content flex-col w-full">
+    <div className="text-center lg:text-left">
+      <h1 className="text-5xl font-bold mb-5 text-orange-600">
+        Please Login!
+      </h1>
+    </div>
+    <div className="card flex-shrink-0 w-full max-w-md bg-slate-100">
+      <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email*</span>
+          </label>
+          <input
+            type="email"
+            {...register("email", { required: true })}
+            name="email"
+            placeholder="email"
+            className="input input-bordered w-full"
+          />
+          {errors.email && <span className="text-red-600">required</span>}
         </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password*</span>
+          </label>
+          <div className="flex items-center ">
+            <input
+              type={show ? "text" : "password"}
+              {...register("password", {
+                required: true,
+              })}
+              placeholder="password"
+              className="input input-bordered w-full"
+            />
+            <span
+              className="absolute right-10 p-2 cursor-pointer mr-2"
+              onClick={() => setShow(!show)}
+            >
+              {show ? <FaEye /> : <FaEyeSlash />}
+            </span>
+          </div>
+
+        </div>
+        <div className="form-control mt-6">
+          <input style={{width:'400px'}}
+            className="btn btn-primary"
+            type="submit"
+            value="Login"
+          />
+        </div>
+      </form>
+      <div className="text-center mb-10">
+        <p>
+          New to this site! Please{" "}
+          <Link to="/signup" className="hover:underline text-blue-500">
+            Register
+          </Link>
+        </p>
+        <SocialLogin></SocialLogin>
       </div>
+    </div>
+  </div>
+</div>
     </>
   );
 };
 
-export default Login;
+export default Login; 
