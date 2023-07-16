afd7b720fbd4510de6e0f967517404398d40051f
jewelrana3 <juyelrana7752@gmail.com>
2023-07-16 11:08:24 +0600

    navber side

--- src/pages/shared/Navber/Navber.jsx
+++ src/pages/shared/Navber/Navber.jsx
@@ -44,7 +44,7 @@ const Navber = ({ color, setColor }) => {
             </li>
           
             {user ? <div className="flex flex-col md:flex-row md:items-center mt-5 md:mt-0" ><span className="tooltip" data-tip={user?.displayName}><img className="w-12 h-12 mb-2 md:mb-0 cursor-pointer bg-cover bg-center mx-4 rounded-full" src={user?.photoURL} alt="" /></span><button onClick={handleLogOut} className="btn btn-outline btn-sm mb-2 text-white">Log Out</button></div> : <Link to='/login'>
-                <button className="btn btn-outline btn-sm">login</button>
+                <button className="btn btn-outline btn-sm text-white">login</button>
             </Link>}
         </>
     // <div className="navItem gap-6 lg:flex ">
