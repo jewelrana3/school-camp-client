0e57545202730d712fbb1f2ffadef5ed24433ca1
jewelrana3 <juyelrana7752@gmail.com>
2023-07-16 11:23:47 +0600

    responsive navber

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
     return (
         <div>
             <Carousel>
-                {/* <div>
+                <div>
                     <img src="https://i.ibb.co/w4YtCpc/omar-ram-e-Eh-Oy-Et-BGMQ-unsplash.jpg" />
                     <div className="absolute top-0 flex items-center px-8 md:px-36 inset-0 bg-gray-900 opacity-75">
                         <div className="text-white">
@@ -15,13 +15,13 @@ const Banner = () => {
                             <Fade delay={1e2} cascade damping={1e-1}><p className="text-xs lg:text-lg text-auto">こんにちは! Embark on a captivating journey into the heart of Japanese. Immerse yourself in the harmony of ancient traditions and cutting-edge technology. Let the beauty of Japanese characters and the allure of its cultural heritage captivate your senses.</p></Fade>
                         </div>
                     </div>
-                </div> */}
+                </div>
                  
                 <div>
                     <img src="https://i.ibb.co/6D3cgwh/alliance-football-club-x1-OStt-M2ajo-unsplash.jpg" />
                     <div className="absolute top-0 flex items-center px-6 md:px-36 inset-0 bg-gray-900 opacity-75">
                         <div className="text-white mx-auto">
-                            <h3 className="-ml-6 text-2xl md:text-3xl md:-ml-6font-semibold mb-4 text-auto"><Fade cascade duration={30}>Experience the Magic of sports Child</Fade></h3>
+                            <h3 className=" text-2xl md:text-3xl md:-ml-6font-semibold mb-4 text-auto"><Fade cascade duration={30}>Experience the Magic of sports Child</Fade></h3>
                             <Fade delay={1e2} cascade damping={1e-1}><p className="text-xs lg:text-lg text-auto">Manchester City boss Guardiola had texted BT Sport pundit Ferdinand earlier to tell him that his side would beat Real Madrid, which they did. Easily.</p></Fade>
                         </div>
                     </div>
@@ -30,7 +30,7 @@ const Banner = () => {
                     <img src="https://i.ibb.co/QbPH9fp/alliance-football-club-z-Lz8k5i-Q1-ZU-unsplash.jpg" />
                     <div className="absolute top-0 flex items-center px-8 md:px-36 inset-0 bg-gray-900 opacity-75">
                         <div className="text-white">
-                            <h2 className="text-2xl md:text-3xl md:-ml-4 font-semibold mb-4 text-auto"><Fade cascade duration={30}>Football match interest Adventure</Fade></h2>
+                            <h2 className="-ml-3 text-2xl md:text-3xl md:-ml-4 font-semibold mb-4 text-auto"><Fade cascade duration={30}>Football match interest Adventure</Fade></h2>
                             <Fade delay={1e2} cascade damping={1e-1}><p className="text-xs lg:text-lg text-slate-300 ">¡Hola! Prepare to embark on a vibrant Spanish adventure filled with passion, excitement, and rich heritage. From the sultry rhythms of Flamenco to the mouthwatering flavors of paella, let Spanish ignite your senses and open doors to a whole new world.</p></Fade>
                         </div>
                     </div>
--- src/pages/shared/Navber/Navber.jsx
+++ src/pages/shared/Navber/Navber.jsx
@@ -44,58 +44,12 @@ const Navber = ({ color, setColor }) => {
             </li>
           
             {user ? <div className="flex flex-col md:flex-row md:items-center mt-5 md:mt-0" ><span className="tooltip" data-tip={user?.displayName}><img className="w-12 h-12 mb-2 md:mb-0 cursor-pointer bg-cover bg-center mx-4 rounded-full" src={user?.photoURL} alt="" /></span><button onClick={handleLogOut} className="btn btn-outline btn-sm mb-2 text-white">Log Out</button></div> : <Link to='/login'>
-                <button className="btn btn-outline btn-sm text-white">login</button>
+                <button className="btn btn-outline btn-sm text-white mb-2">login</button>
             </Link>}
         </>
-    // <div className="navItem gap-6 lg:flex ">
-
-    //     <li><Link className='mr-6 text-xl ' onClick={handleColor}>{<FaMoon className="sm:hidden"></FaMoon>}</Link></li>
-    //     <li><NavLink to='/'>Home</NavLink></li>
-    //     <li><NavLink to='/All-instructor'>Instructor</NavLink></li>
-    //     <li><NavLink to='/classes'>Classes</NavLink></li>
-    //     <li><NavLink to='/dashboard'>Dashboard</NavLink></li>
-
-
-    // </div>
-
+    
     return (
-        // <div className="navbar bg-slate-600 fixed z-10 mx-auto opacity-80 text-white w-[95%]">
-        //     <div className="navbar-start">
-        //         <div className="dropdown">
-        //             <label tabIndex={0} className="btn btn-ghost lg:hidden">
-        //                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-        //             </label>
-        //             <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-black rounded-box w-52 ">
-        //                 {navItem}
-        //             </ul>
-        //         </div>
-
-        //         <div className="flex items-center">
-        //             <span>
-        //                 <img style={{ borderRadius: '50px' }} className="w-12" src="https://i.ibb.co/chxbvxW/Untitled2.png" alt="photo" />
-        //             </span>
-
-        //             <span className="text-orange-600 ml-4 text-2xl">Sports</span>{" "}
-        //             <span className="text-blue-600 text-2xl ml-2">Masic</span>
-        //         </div>
-        //     </div>
-
-        //     <div className="navbar-end">
-        //         <div className="navbar-center hidden lg:flex ">
-        //             <ul className="menu menu-horizontal px-1 ">
-        //                 {navItem}
-        //             </ul>
-        //         </div>
-        //         {
-        //             user?.email ? <>
-        //                 <img title={user?.displayName} style={{ width: '40px', borderRadius: '50%' }} src={user?.photoURL} alt="" />
-        //                 <button onClick={handleLogOut} className="btn btn-ghost">LogOut</button>
-        //             </> : <>
-        //                 <NavLink to="/login" className="btn btn-error">Login</NavLink>
-        //             </>
-        //         }
-        //     </div >
-        // </div >
+     
         <div className="drawer">
             <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content flex flex-col">
